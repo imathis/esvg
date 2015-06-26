4b0f71344476efeca50671ad8c94e5b083695d58
Brandon Mathis <brandon@imathis.com>
2015-06-26 14:00:38 -0500

    Now supporting Rails mode where configuration defaults are Rails friendly

--- lib/esvg.rb
+++ lib/esvg.rb
@@ -4,21 +4,25 @@ require "svg_optimizer"
 module Esvg
   extend self
 
-  attr_accessor :config, :files
+  attr_accessor :files
+
+  CONFIG = {
+    path: Dir.pwd,
+    namespace: 'icon',
+    namespace_after: true,
+    font_size: '1em',
+    stylesheet_embed: false,
+    output_path: Dir.pwd
+  }
+
+  CONFIG_RAILS = {
+    path: "app/assets/images/svg_icons",
+    css_path: "app/assets/stylesheets/_svg-icons.scss",
+    html_path: "app/views/shared/_svg-icons.html"
+  }
 
   def optimize(options)
-    @config = {
-      dir: Dir.pwd,
-      namespace: 'icon',
-      namespace_after: true,
-      font_size: '1em',
-    }.merge(options)
-
-    config[:output] = if config[:filename]
-      File.expand_path(config[:filename])
-    else
-      File.join(config[:dir],config[:output])
-    end
+    config(options)
 
     @files = {}
 
@@ -26,23 +30,50 @@ module Esvg
       svg = File.read(f)
       @files[File.basename(f, ".*")] = SvgOptimizer.optimize(svg)
     end
-    
-    File.open(config[:output], 'w') do |io|
-      io.write(output)
+
+    if config[:stylesheet_embed]
+      FileUtils.mkdir_p(File.expand_path(File.dirname(config[:css_path])))
+
+      File.open(config[:css_path], 'w') do |io|
+        io.write(stylesheet_embed)
+      end
+    else
+      FileUtils.mkdir_p(File.expand_path(File.dirname(config[:html_path])))
+
+      File.open(config[:html_path], 'w') do |io|
+        io.write(html)
+      end
     end
   end 
 
-  def output
-    if config[:css]
-      css
-    elsif config[:html]
-      html
-    else
-      abort 'no options'
+  def config(options={})
+    @config ||= begin
+      paths = [options[:config_file], 'config/esvg.yml', 'esvg.yml'].compact
+
+      config = if path = paths.select{ |p| File.exist?(p)}.first
+        CONFIG.merge(symbolize_keys(YAML.load(File.read(path) || {})))
+      else
+        CONFIG
+      end
+
+      config.merge!(CONFIG_RAILS) if options[:rails]
+      config.merge(options)
+
+      config[:css_path]  ||= config[:output_path]
+      config[:html_path] ||= config[:output_path]
+      config.delete(:output_path)
+
+      config
     end
   end
 
-  def css
+  def symbolize_keys(hash)
+    h = {}
+    hash.each {|k,v| h[k.to_sym] = v }
+    h
+  end
+
+  def stylesheet_embed
     styles = []
     preamble = %Q{%svg-icon { 
   clip: auto;
@@ -71,6 +102,29 @@ module Esvg
     styles.join("\n")
   end
 
+  def html
+    svg = []
+    svg << %Q{<svg class="icon-symbols" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" style="display:none">}
+    files.each do |name, contents|
+      svg << contents.gsub(/<svg.+?>/, %Q{<symbol id="#{property_name(name)}" #{dimensions(contents)}>})  # convert svg to symbols
+                     .gsub(/<\/svg/, '</symbol')     # convert svg to symbols
+                     .gsub(/style=['"].+?['"]/, '')  # remove inline styles
+                     .gsub(/\n/, '')                 # remove endlines
+                     .gsub(/\s{2,}/, ' ')            # remove whitespace
+                     .gsub(/>\s+</, '><')            # remove whitespace between tags
+    end
+    svg << %Q{</svg>}
+    svg.join("\n")
+  end
+
+  def dimensions(input)
+    dimensions = [] 
+    %w(viewBox).map do |dimension|
+        dimensions << input.scan(/<svg.+(#{dimension}=["'].+?["'])/).flatten.first
+    end
+    dimensions.compact.join(' ')
+  end
+
   def property_name(file)
     name = dasherize(file)
     if config[:namespace_after]
@@ -80,32 +134,12 @@ module Esvg
     end
   end
 
-  def html
-    svg = []
-    svg << %Q{<svg xmlns="http://www.w3.org/2000/svg" width="312"><defs>}
-    files.each do |name, contents|
-      svg << contents.gsub(/<svg.+?>/, %Q{<g id="#{property_name(name)}" #{dimensions(contents)}>})
-                     .gsub(/<\/svg/, '</g')
-                     .gsub(/\n/, '')
-    end
-    svg << %Q{</defs></svg>}
-    svg.join("\n")
-  end
-
-  def dimensions(input)
-    dimensions = [] 
-    %w(viewBox height width).map do |dimension|
-        dimensions << input.scan(/<svg.+(#{dimension}=["'].+?["'])/).flatten.first
-    end
-    dimensions.compact.join(' ')
-  end
-
   def dasherize(input)
     input.gsub(/\W/, '-').gsub(/-+/, '-')
   end
 
   def find_files
-    path = File.join(config[:dir], '*.svg')
+    path = File.join(config[:path], '*.svg')
 
     Dir[path].uniq
   end
