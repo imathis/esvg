89239cea86f22f1de1197bf60f45d10454c433da
Brandon Mathis <brandon@imathis.com>
2015-12-07 21:43:12 -0600

    Fixed test file name

--- test/use_icon.rb
+++ test/use_icon.rb
@@ -1,11 +1,15 @@
 require 'esvg'
+require 'fileutils'
+
 Esvg.icons({config_file: '_alias.yml', path: 'svg_icons'})
 use = Esvg.svg_icon('chat-bubble')
 
 def write_file(path, contents)
-  FileUtils.mkdir_p(File.expand_path(File.dirname(path)))
+  path = File.expand_path(path)
+  FileUtils.mkdir_p(File.dirname(path))
   File.open(path, 'w') do |io|
     io.write(contents)
+    puts "written to #{path}"
   end
 end
 
